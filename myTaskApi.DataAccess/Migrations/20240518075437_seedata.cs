da95c4c9bffffcdf77f7131d5d903ee5359d7a2d
Manoj wickramasingha <141001887+ManojWickramasingha@users.noreply.github.com>
2024-05-18 15:17:06 +0530

    Add SqlServer Database and dependency injection using create database and dummy data inserted.

---
+++ myTaskApi.DataAccess/Migrations/20240518075437_seedata.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace myTaskApi.DataAccess.Migrations
+{
+    public partial class seedata : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ myTaskApi.DataAccess/TodoDbContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using myTaskApi.Model;
+
+namespace myTaskApi.DataAccess
+{
+    public class TodoDbContext : DbContext
+    {
+        public DbSet<Todo> Todos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = "Server=DESKTOP-DV7M2UV; Database = myTodoDB; user=sa; password=manoj@123";
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Todo>().HasData(new Todo
+            {
+                id = 10,
+                title = "get book for school from DB",
+                Description = "Get some text books for school",
+                created = DateTime.Now,
+                Due = DateTime.Now.AddDays(5),
+                status = TodoStatus.New,
+            });
+        }
+    }
+}
